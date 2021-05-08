4a5ccb2ae0fb9cfa372d848cadac6cf74b475e17
huangzhubin <huangzhubin@able-elec.com>
2021-05-08 09:55:35 +0800

    #支付宝支付异步回调需要能解析的地址

--- src/main/java/com/zbhuang/zhs/controller/OrderController.java
+++ src/main/java/com/zbhuang/zhs/controller/OrderController.java
@@ -5,9 +5,11 @@ import com.zbhuang.zhs.bean.AlipayBean;
 import com.zbhuang.zhs.service.PayService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /* 订单接口 */
 @RestController
@@ -16,7 +18,7 @@ public class OrderController {
   @Resource
   private PayService payService;//调用支付服务​
   /*阿里支付*/
-  @PostMapping(value = "alipay")
+  @PostMapping(value = "/alipay")
   public String alipay(String out_trade_no,String subject,String total_amount,String body) throws AlipayApiException {
 
     return payService.aliPay(new AlipayBean()
@@ -25,4 +27,11 @@ public class OrderController {
          .setTotal_amount(new StringBuffer().append(total_amount))
          .setSubject(subject));
    }
+
+  /*阿里支付*/
+  @ResponseBody
+  @RequestMapping("/notifyUrl")
+  public void notifyUrl(HttpServletRequest request) throws Exception {
+    System.out.println("========================================支付回调=====================================");
+  }
 }
